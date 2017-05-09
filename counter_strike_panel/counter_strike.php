e556c7c40c96a8a2e1d515179aa1f145702d48ee
keddyboys <keddyboys@gmail.com>
2017-05-09 21:20:51 +0300

    Update

---
+++ counter_strike_panel/counter_strike.php
@@ -0,0 +1,27 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| https://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: counter_strike_panel.php
+| Author: Keddy
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+require_once file_exists('maincore.php') ? 'maincore.php' : __DIR__."/../../maincore.php";
+if (!db_exists(DB_SERVER)) {
+    redirect(BASEDIR."error.php?code=404");
+}
+require_once THEMES."templates/header.php";
+include_once CS_INCLUDES."counter.php";
+Servers::getInstance(TRUE)->get_server_list();
+require_once THEMES."templates/footer.php";
+
+?>
